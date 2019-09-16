8beeaa76670a9656ea151b013da520e9b83de2a7
Adam Kariv <adam.kariv@gmail.com>
2019-09-16 16:21:52 +0300

    workaround detached head

--- tools/deploy.sh
+++ tools/deploy.sh
@@ -2,7 +2,8 @@
 echo 'DEPLOYING'
 export REPO=hasadna/reportit-scripts
 . tools/prepare_github.sh
+git checkout -b __deploy
 find src -name script.json | xargs git add
 git status
 git commit -m "Auto update of json scripts"
-git push -v
+git push -v origin __deploy:master
