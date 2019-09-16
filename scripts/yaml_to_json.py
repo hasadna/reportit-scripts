75ab3c9e3d6d80f448f6edf86dd7db88224d0993
Adam Kariv <adam.kariv@gmail.com>
2019-09-16 14:50:16 +0300

    Push & pull from transifex, enable travis

--- scripts/yaml_to_json.py
+++ scripts/yaml_to_json.py
@@ -1,9 +1,11 @@
+import os
 from pathlib import Path
 from hashlib import md5
+from slugify import slugify
 import glob
 import yaml
 import json
-
+import requests
 
 def calc_hash(s):
     ret = md5(s.encode('utf8')).hexdigest()[:10]
@@ -49,13 +51,112 @@ def assign_ids(x, stack=[]):
     else:
         return
 
+TRANSIFEX_TOKEN = os.environ.get('TRANSIFEX_TOKEN')
+LANGUAGES = ('ar', 'am', 'en', 'ru')
+
+def assign_translations(x, stack, lang=None, translations=None):
+    if isinstance(x, dict):
+        key = None
+        if x.get('name'):
+            stack.append(slugify(x['name']))
+        if 'uid' in x:
+            stack.append(x['uid'][:2])
+        for k, v in x.items():
+            if k == 'steps':
+                for index, s in enumerate(v):
+                    new_stack = stack + []
+                    yield from assign_translations(s, new_stack, lang, translations)
+            elif k in ('say', 'show'):
+                key = '/'.join(str(s) for s in stack)
+                yield key, v
+            else:
+                yield from assign_translations(v, stack[:], lang, translations)
+        if lang is not None and key in translations:
+            x.setdefault('.tx', {})[lang] = translations[key]
+    elif isinstance(x, list):
+        for index, xx in enumerate(x):
+            new_stack = stack + [index]
+            yield from assign_translations(xx, new_stack, lang, translations)
+
+
+def transifex_session():
+    s = requests.Session()
+    s.auth = ('api', TRANSIFEX_TOKEN)
+    return s
+
+
+def transifex_slug(filename):
+    return '_'.join(filename.parts).replace('.', '_')
+
+
+def push_translations(filename: Path, translations):
+    translations = dict(he=translations)
+    content = yaml.dump(translations, allow_unicode=True, indent=2, width=1000000)
+
+    slug = transifex_slug(filename)
+    s = transifex_session()
+    resp = s.get(f'https://www.transifex.com/api/2/project/equalityorgil/resource/{slug}/')
+
+    if resp.status_code == requests.codes.ok:
+        print('Update file:')
+        data = dict(
+            content=content,
+        )
+
+        resp = s.put(
+            f'https://www.transifex.com/api/2/project/equalityorgil/resource/{slug}/content/',
+            json=data
+        )
+        print(resp.status_code, resp.content[:50])
+        
+    else:
+        print('New file:', slug)
+        data = dict(
+            slug=slug,
+            name=str(filename),
+            accept_translations=True,
+            i18n_type='YAML_GENERIC',
+            content=content,
+        )
+
+        resp = s.post(
+            'https://www.transifex.com/api/2/project/equalityorgil/resources/',
+            json=data
+        ) 
+        print(resp.status_code, resp.content[:50])
+
+
+def pull_translations(lang, filename):
+    s = transifex_session()
+    slug = transifex_slug(filename)
+    translations = s.get(
+            f'https://www.transifex.com/api/2/project/equalityorgil/resource/{slug}/translation/{lang}/',
+    ).json()
+    translations = yaml.load(translations['content'], Loader=yaml.BaseLoader)['he']
+    translations = dict((k, v) for k, v in translations.items() if v)
+    return translations
+
 
 if __name__=='__main__':
     files = Path().glob('src/*/script.yaml')
     for f_in in files:
         print(f_in)
-        s = yaml.load(f_in.open())
-        assign_ids(s, [str(f_in)])
-        s = dict(s=s)
+        scripts = yaml.load(f_in.open())
+        assign_ids(scripts, [str(f_in)])
+
+        if TRANSIFEX_TOKEN:
+            translations = {}
+            for script in scripts:
+                for k, v in assign_translations(script, []):
+                    assert k not in translations
+                    translations[k] = v
+            push_translations(f_in, translations)
+            for lang in LANGUAGES: 
+                translations = pull_translations(lang, f_in)
+                for script in scripts:
+                    for k, v in assign_translations(script, [], lang, translations):
+                        pass
+
+        scripts = dict(s=scripts)
         f_out = f_in.with_suffix('.json')
-        json.dump(s, f_out.open('w'), ensure_ascii=False, sort_keys=True)
+        json.dump(scripts, f_out.open('w'), ensure_ascii=False, sort_keys=True)
