67ba4049f0a445e046856a157bff801f2871a51a
Adam Kariv <adam.kariv@gmail.com>
2019-12-12 14:16:30 +0200

    Fix datapackage.tx extension in deploy

--- tools/deploy.sh
+++ tools/deploy.sh
@@ -4,7 +4,7 @@ export REPO=hasadna/reportit-scripts
 . tools/prepare_github.sh
 git checkout -b __deploy
 find src -name script.json | xargs git add
-find src -name \*.tx.datapackage.json | xargs git add
+find src -name \*.datapackage.tx.json | xargs git add
 git status
 git commit -m "Auto update of json sources"
 git push -v origin __deploy:master
