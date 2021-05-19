468ef8f3e614b4396d0347705b8027101fb94374
Adam Kariv <adam.kariv@gmail.com>
2021-05-19 14:56:46 +0300

    passphrase

--- tools/prepare_github.sh
+++ tools/prepare_github.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-openssl aes-256-cbc -k "$travis_key_password" -d -md sha256 -a -in travis_key.enc -out travis_key
+openssl aes-256-cbc --pass ""  -k "$travis_key_password" -d -md sha256 -a -in travis_key.enc -out travis_key
 echo "Host github.com" > ~/.ssh/config
 echo "  IdentityFile  $(pwd)/travis_key" >> ~/.ssh/config
 chmod 400 travis_key
